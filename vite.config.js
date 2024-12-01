c77598f410293e1dcf7f048214021b375c720941
Shohan Islam Joy <shohanislamjoy@gmail.com>
2024-12-01 12:13:06 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        // target: "http://localhost:8000", api test in netlify
-        target:"https://vuejs-server-test-site.netlify.app/api",
+        target: "http://localhost:8000"
         changeOrigin: true,
         rewrite:(path)=>path.replace(/^\/api/,""),
 
