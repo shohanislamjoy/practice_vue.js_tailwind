12555bec3d71c8077b62e98dc6d43839462c93b2
Shohan Islam Joy <shohanislamjoy@gmail.com>
2024-12-01 14:39:20 +0600

    online render server activated

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        // target: "http://localhost:8000",
+        target: "online render server activated",
         changeOrigin: true,
         rewrite:(path)=>path.replace(/^\/api/,""),
 
